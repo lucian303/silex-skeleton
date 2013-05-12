8f54a7e97d23761a5afea05ec52ca5b643d9db2a
Lucian Hontau <lucian303@gmail.com>
2013-05-11 20:57:35 -0700

    Restructuring controllers and views into modules. Want to keep this simple and flexible instead of registering actualy controller classes as services (might as well use sy2 then)

--- app/app.php
+++ app/app.php
@@ -2,6 +2,6 @@
 
 /** @var $app Silex\Application */
 $app = require_once 'init.php';
-$app = require_once 'controllers.php';
+$app = require_once 'modules.php';
 
 $app->run();
--- app/init.php
+++ app/init.php
@@ -6,7 +6,6 @@ use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\SecurityServiceProvider;
 use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
-use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Monolog\Logger;
@@ -17,12 +16,12 @@ $app = new Silex\Application();
 // TODO: Set this based on config
 $app['debug'] = true;
 
-//$app->register(new HttpCacheServiceProvider());
-//$app->register(new SessionServiceProvider());
-//$app->register(new ValidatorServiceProvider());
-//$app->register(new FormServiceProvider());
-//$app->register(new UrlGeneratorServiceProvider());
-//
+$app->register(new HttpCacheServiceProvider());
+$app->register(new SessionServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new FormServiceProvider());
+$app->register(new UrlGeneratorServiceProvider());
+
 // $app->register(new SecurityServiceProvider(), array(
 //     'security.firewalls' => array(
 //         'admin' => array(
@@ -50,16 +49,8 @@ $app->register(new TranslationServiceProvider(array(
 $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../log/app.log',
     'monolog.name'    => 'app',
-    'monolog.level'   => Logger::INFO,
-));
-
-$app->register(new TwigServiceProvider(), array(
-    'twig.options'        => array(
-        'cache'            => isset($app['twig.options.cache']) ? $app['twig.options.cache'] : false,
-        'strict_variables' => true
-    ),
-    'twig.form.templates' => array('form_div_layout.html.twig', 'templates/form_div_layout.html.twig'),
-    'twig.path'           => array(__DIR__ . '/views'),
+    'monolog.level'   => 300,
+//    'monolog.level'   => Logger::INFO,
 ));
 
 $app->register(new Silex\Provider\DoctrineServiceProvider());
