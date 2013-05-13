dc519e933b1414b3d5aae774d23162a6b24d8583
Lucian Hontau <lucian303@gmail.com>
2013-05-12 21:28:17 -0700

    Cleanup

--- app/modules/api/controllers/api.php
+++ app/modules/api/controllers/api.php
@@ -1,12 +1,4 @@
 <?php
 
-$app->get('/api/search/{keyword}', function($keyword) use($app) {
-    return $app->json(array('keyword: '=> $keyword, 'result' => 'blah', 'subobject' => array('subkey' => 'subvalue')));
-});
-
-$app->post('/api/add/{keyword}', function() use ($app) {
-	return $app->json(array('result' => 'Item posted'));
-});
-
 /** @return $app Silex\Application */
 return $app;
