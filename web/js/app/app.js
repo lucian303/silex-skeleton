388b0243c61d57ed13ab529a75c54ed3388b22a8
Lucian Hontau <lucian303@gmail.com>
2013-06-14 01:08:56 -0700

    Example jQuery ajax call to the API.

--- web/js/app/app.js
+++ web/js/app/app.js
@@ -1,12 +1,13 @@
 // app.js
 (function($) {
 	$(document).ready(function() {
-//		$('#button-ajax-test').click(function() {
-//			$.ajax('/api/search/lucian', {
-//				success: function(data) {
-//					console.log(data);
-//				}
-//			});
-//		});
+		$('#button-ajax-test').click(function(event) {
+			console.log(event);
+			$.ajax('/api/search/lucian', {
+				success: function(data) {
+					console.log(data);
+				}
+			});
+		});
 	});
 })(jQuery);
